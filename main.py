32d346185a2df67887d2db7b1ad4bc0182a798f6
BytePedaler <95730407+BytePedaler@users.noreply.github.com>
2024-07-29 21:58:31 -0700

    Created time intervals and blueprint for sensor data.

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
+import time
+SECONDS_PER_MINUTE = 60
+
+def time_interval():
+     min_interval = SECONDS_PER_MINUTE * 0.5
+     return min_interval
+
 def sensor_acquisition():
     while True:
-        pass
-
+        print(time.localtime())
+        time.sleep(time_interval())
 
 if __name__ == '__main__':
-    pass
+    sensor_acquisition()
