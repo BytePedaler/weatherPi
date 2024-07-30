805b192642758111028f3b713a51caf558a1e142
BytePedaler <95730407+BytePedaler@users.noreply.github.com>
2024-07-29 22:27:21 -0700

    Improved optimization.

--- main.py
+++ main.py
@@ -8,12 +8,16 @@ from pms5003 import PMS5003
 # Const:
 SECONDS_PER_MINUTE = 60
 
-# Sensors:
+# Sensor initialization:
 bus = SMBus(1)
 bme280 = BME280(i2c_dev=bus)
 ltr = LTR559()
-gas_readings = gas.read_all()
 pms5003 = PMS5003()
+current_temp = bme280.get_temperature()
+current_pressure = bme280.get_pressure()
+current_humidity = bme280.get_humidity()
+current_light = ltr.get_lux()
+gas_readings = gas.read_all()
 part_mat_readings = pms5003.read()
 
 
@@ -21,15 +25,17 @@ def time_interval():
      min_interval = SECONDS_PER_MINUTE * 0.5
      return min_interval
 
+def sensor_readings():
+    print("Current temperature: " + str(current_temp))
+    print("Current pressure: " + str(current_pressure))
+    print("Current humidity: " + str(current_humidity))
+    print("Current light level: " + str(current_light))
+    print("Current gas: " + str(gas_readings))
+    print("Current particulates: " + str(part_mat_readings))
+
 def sensor_acquisition():
     while True:
-        # Sensors:
-        print(bme280.get_pressure())
-        print(bme280.get_humidity())
-        print(ltr.get_lux())
-        print(gas_readings)
-        print(part_mat_readings)
-
+        sensor_readings()
         # Time:
         print(time.localtime())
         time.sleep(time_interval())
