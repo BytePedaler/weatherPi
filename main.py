1834785ac3b13757a7894970cf3d7b162aadae8d
BytePedaler <95730407+BytePedaler@users.noreply.github.com>
2024-07-29 22:06:19 -0700

    Improved readability.

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ gas_readings = gas.read_all()
 pms5003 = PMS5003()
 part_mat_readings = pms5003.read()
 
+
 def time_interval():
      min_interval = SECONDS_PER_MINUTE * 0.5
      return min_interval
@@ -23,7 +24,8 @@ def time_interval():
 def sensor_acquisition():
     while True:
         # Sensors:
-        print(bme280.get_pressure(), bme280.get_humidity())
+        print(bme280.get_pressure())
+        print(bme280.get_humidity())
         print(ltr.get_lux())
         print(gas_readings)
         print(part_mat_readings)
@@ -31,6 +33,7 @@ def sensor_acquisition():
         # Time:
         print(time.localtime())
         time.sleep(time_interval())
+
 
 if __name__ == '__main__':
     sensor_acquisition()
