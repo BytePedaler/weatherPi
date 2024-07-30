f591ca0c8a3de95ba8a71a6383890bf3e6afc9e9
BytePedaler <95730407+BytePedaler@users.noreply.github.com>
2024-07-29 22:03:51 -0700

    Created sensor data imports and readings.

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 import time
+from smbus2 import SMBus
+from bme280 import BME280
+from ltr559 import LTR559
+from enviroplus import gas
+from pms5003 import PMS5003
+
+# Const:
 SECONDS_PER_MINUTE = 60
+bus = SMBus(1)
+bme280 = BME280(i2c_dev=bus)
+ltr = LTR559()
+gas_readings = gas.read_all()
+pms5003 = PMS5003()
+part_mat_readings = pms5003.read()
 
 def time_interval():
      min_interval = SECONDS_PER_MINUTE * 0.5
@@ -7,6 +20,13 @@ def time_interval():
 
 def sensor_acquisition():
     while True:
+        # Sensors:
+        print(bme280.get_pressure(), bme280.get_humidity())
+        print(ltr.get_lux())
+        print(gas_readings)
+        print(part_mat_readings)
+
+        # Time:
         print(time.localtime())
         time.sleep(time_interval())
 
